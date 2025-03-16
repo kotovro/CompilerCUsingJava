8519397e87b644fff20aa45f347f678d2750002f
Kotvoro <kotvoro@gmail.com>
2025-03-16 12:13:46 +0300

    Implements unary operations(post increment and post decrement)

--- CTreeJavaCC/src/main/java/ru/vsu/cs/course3/compiler/ast/AssignNode.java
+++ CTreeJavaCC/src/main/java/ru/vsu/cs/course3/compiler/ast/AssignNode.java
@@ -1,17 +1,12 @@
 package ru.vsu.cs.course3.compiler.ast;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class AssignNode implements StmtNode {
-    private IdentNode ident;
+    private IdentNode ident = null;
     private ExprNode expr = null;
-    ///adds TypeNode
     private TypeNode type = null;
 
-    ///added TypeNode cosntructor
     public AssignNode(TypeNode type, IdentNode ident, ExprNode expr) {
         this.type = type;
         this.ident = ident;
@@ -31,8 +26,7 @@ public class AssignNode implements StmtNode {
     public AssignNode(IdentNode ident) {
         this.ident = ident;
     }
-    /// modified it from simple return Arrays.asList(ident, expr)
-    /// By doing several if statements, we don't have to modify tree printig method - otherwise we had to add null check fo r children
+
     @Override
     public Collection<AstNode> childs() {
         List<AstNode> children = new ArrayList<AstNode>();
